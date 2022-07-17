ac34888d4a6e03d23f396169cb588280708139e1
GarboMuffin <muffin@muffin.ink>
2022-07-17 02:32:58 -0500

    Refactor tests

--- tests/ts-es6.ts
+++ tests/ts-es6.ts
@@ -1,34 +1,4 @@
 import VM from 'scratch-vm';
 
 const vm = new VM();
-const runtime = vm.runtime;
-
-runtime.on('SCRIPT_GLOW_ON', (glowData) => {
-  const id: string = glowData.id;
-});
-vm.on('SCRIPT_GLOW_OFF', (glowData) => {
-  const id: string = glowData.id;
-});
-
-vm.emit('targetsUpdate', {
-  targetList: [],
-  editingTarget: 'abc 123'
-});
-
-const target = vm.runtime.getSpriteTargetByName('Sprite1');
-if (target) {
-  const variable = target.lookupVariableByNameAndType('test', 'list');
-  if (variable) {
-    const name: string = variable.name;
-    variable.value.filter(i => i.toString());
-  }
-
-  target.lookupOrCreateVariable(')(!@*#)!(@#*()', 'my variable').value;
-
-  // @ts-expect-error
-  target.lookupVariableById('#@)$(*%)(#').id;
-
-  target.effects.ghost += 10;
-} else {
-  const doesNotExist: undefined = target;
-}
+vm.clear();
