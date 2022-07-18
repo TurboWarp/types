2e743a6b2d513810a6bae192f61100801a651671
GarboMuffin <muffin@muffin.ink>
2022-07-17 21:14:55 -0500

    Document what GUIScratchStorage is

--- types/scratch-storage.d.ts
+++ types/scratch-storage.d.ts
@@ -8,6 +8,9 @@ declare class ScratchStorage {
   // TODO
 }
 
+/**
+ * Modified version of ScratchStorage used by scratch-gui.
+ */
 declare class GUIScratchStorage extends ScratchStorage {
   projectToken?: string;
   setProjectToken(token: string): void;
