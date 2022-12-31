81f6c4711e0c6b01cf7b736709eb58332ba38169
GarboMuffin <muffin@mailbox.org>
2022-12-30 22:53:18 -0600

    Add types for scratch-vm extensions
    From TurboWarp/extensions.

---
+++ tests/test-scratch-vm-extension.js
@@ -0,0 +1,36 @@
+(function(Scratch) {
+  'use strict';
+
+  class Fetch {
+    getInfo () {
+      return {
+        id: 'fetch',
+        name: 'Fetch',
+        blocks: [
+          {
+            opcode: 'get',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'GET [URL]',
+            arguments: {
+              URL: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'https://extensions.turbowarp.org/hello.txt'
+              }
+            }
+          }
+        ]
+      };
+    }
+
+    /**
+     * @param {{URL: string;}} args
+     */
+    get (args) {
+      return fetch(args.URL)
+        .then(r => r.text())
+        .catch(() => '');
+    }
+  }
+
+  Scratch.extensions.register(new Fetch());
+})(Scratch);
